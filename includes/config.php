98b6c3549ff320838bb4450f126ce4a6bb512bcd
nikhilbariki332 <68799293+nikhilbariki332@users.noreply.github.com>
2020-11-17 13:04:29 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
 // Database Configuration
 
-$dbHost = "ec2-54-147-126-202.compute-1.amazonaws.com";
+$dbHost = "https://ec2-54-147-126-202.compute-1.amazonaws.com";
 $dbUser = "akyrdaqzzaeqia";
 $dbPW = "afad8efb1a034ec769aef9bb8b5d28f863a65c1ab5b4a547562e5cd8aed09580";
 $dbName = "dcj0paevvf1cd0";
